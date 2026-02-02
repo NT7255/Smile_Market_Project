b305d84b633e24c81e823b881818a5d3ec798835
Teemarot <gastongaming123@gmail.com>
2026-02-02 17:37:15 +0700

    Add files via upload

--- repassword.php
+++ repassword.php
@@ -9,6 +9,47 @@
 </head>
 <body>
     <?php include "tab-above-user1.php"; ?> <!-- แทบบน อย่าแก้ By เติ้ง --> <!--โหลดไฟล์ tab-above-user1.php ด้วย-->
-    หน้ารีเซ็ตรหัสผ่าน
+    <main class="forgot-container">
+    <h2 class="forgot-title">กรอกเพื่อเปลี่ยนรหัสผ่าน</h2>
+
+    <form class="forgot-form">
+
+        <!-- อีเมล / เบอร์โทร -->
+        <div class="form-group">
+            <label>อีเมล หรือเบอร์โทรของคุณ</label>
+            <input type="text" placeholder="teemarot123@example.com">
+        </div>
+
+        <!-- รหัสผ่าน -->
+        <div class="row">
+            <div class="form-group">
+                <label>รหัสผ่าน</label>
+                <input type="password" placeholder="********">
+            </div>
+
+            <div class="form-group">
+                <label>ยืนยันรหัสผ่าน</label>
+                <input type="password" placeholder="********">
+            </div>
+        </div>
+
+        <!-- OTP -->
+        <div class="form-group">
+            <label>ยืนยันรหัสจาก SMS</label>
+
+            <div class="sms-row">
+                <input type="text" placeholder="กรอก">
+                <button type="button" class="otp-btn">ขอรหัสจาก SMS</button>
+            </div>
+        </div>
+
+        <!-- ปุ่ม -->
+        <div class="action-buttons">
+            <button type="button" class="cancel-btn">ยกเลิก</button>
+            <button type="submit" class="confirm-btn">ตกลง</button>
+        </div>
+
+    </form>
+</main>
 </body>
 </html>
